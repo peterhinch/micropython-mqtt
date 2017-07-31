857abb1b225bdd90d10823ede40908518c885ca5
Peter Hinch <peter@hinch.me.uk>
2017-07-31 11:45:44 +0100

    Update code comment in net_local.py.

--- net_local.py
+++ net_local.py
@@ -1,10 +1,10 @@
 # net_local.py Local network parameters for Pyboard MQTT link
-# Edit for your network. See README.md for this structure.
+# Edit for your network. See NO_NET.md for this structure.
 
 # Author: Peter Hinch.
 # Copyright Peter Hinch 2017 Released under the MIT license.
 
-# See README.md para 2.3.1
+# See NO_NET.md para 2.3.1
 
 INIT = ('init', 'my_SSID', 'my_password', 'broker_ip_address', '', '', repr({}),
                  1, 0, 0, 1, 3600, 60)
