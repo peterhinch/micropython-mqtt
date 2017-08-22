c98701d752e246fde54ebc4f6952d8c57557bd1f
Peter Hinch <peter@hinch.me.uk>
2017-08-22 17:51:13 +0100

    Change code comments in ssl.py to reflect its broken status.

--- mqtt_as/ssl.py
+++ mqtt_as/ssl.py
@@ -1,13 +1,10 @@
-# clean.py Test of asynchronous mqtt client with clean session.
+# ssl.py Test of asynchronous mqtt client with SSL. Doesn't actually work.
+# Please help me fix it...
+
 # (C) Copyright Peter Hinch 2017.
 # Released under the MIT licence.
 
 # Public brokers https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
-
-# The use of clean_session means that after a connection failure subscriptions
-# must be renewed (MQTT spec 3.1.2.4). This is done by the connect handler.
-# Note that publications issued during the outage will be missed. If this is
-# an issue see unclean.py.
 
 # red LED: ON == WiFi fail
 # blue LED heartbeat: demonstrates scheduler is running.
@@ -60,10 +57,10 @@ config['server'] = SERVER
 config['connect_coro'] = conn_han
 config['wifi_coro'] = wifi_han
 config['ssl'] = True
+# config['ssl_params'] = {'certfile': '/mosquitto.org.crt'}
 
 # Set up client
 MQTTClient.DEBUG = True  # Optional
-ssl_params = {} #{'certfile': '/mosquitto.org.crt'}
 client = MQTTClient(config)
 
 loop = asyncio.get_event_loop()
