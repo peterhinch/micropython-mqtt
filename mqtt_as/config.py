a98d006dac82faf207eb0eacdf9f281b4a6cc066
Thijs Triemstra <info@collab.nl>
2018-07-25 16:35:17 +0200

    also check for esp32_LoBo in config.py

--- mqtt_as/config.py
+++ mqtt_as/config.py
@@ -1,8 +1,7 @@
-from mqtt_as import config
-from sys import platform
+from mqtt_as import config, ESP32
 
 # Include any cross-project settings.
 
-if platform == 'esp32':
+if ESP32:
     config['ssid'] = 'my SSID'  # EDIT if you're using ESP32
     config['wifi_pw'] = 'my WiFi password'
