b36d40a553454a99f5e2453ddf6b4ca88cf485e1
Peter Hinch <peter@hinch.me.uk>
2024-06-19 14:36:42 +0100

    range.py, range_ex.py Correct code comments. Format with black.

--- mqtt_as/range.py
+++ mqtt_as/range.py
@@ -1,16 +1,16 @@
 # range.py Test of asynchronous mqtt client with clean session False.
-# (C) Copyright Peter Hinch 2017-2022.
+# (C) Copyright Peter Hinch 2017-2024.
 # Released under the MIT licence.
 
 # Now uses the event interface
 
 # Public brokers https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
 
-# This demo is for wireless range tests. If OOR the red LED will light.
+# This demo is for wireless range tests. If OOR the red WiFi ON LED will go out.
 # In range the blue LED will pulse for each received message.
 # Uses clean sessions to avoid backlog when OOR.
 
-# red LED: ON == WiFi fail
+# red LED: ON == WiFi OK
 # blue LED pulse == message received
 # Publishes connection statistics.
 
@@ -18,19 +18,22 @@ from mqtt_as import MQTTClient
 from mqtt_local import wifi_led, blue_led, config
 import uasyncio as asyncio
 
-TOPIC = 'shed'  # For demo publication and last will use same topic
+TOPIC = "shed"  # For demo publication and last will use same topic
 
 outages = 0
+
 
 async def pulse():  # This demo pulses blue LED each time a subscribed msg arrives.
     blue_led(True)
     await asyncio.sleep(1)
     blue_led(False)
 
+
 async def messages(client):
     async for topic, msg, retained in client.queue:
         print(f'Topic: "{topic.decode()}" Message: "{msg.decode()}" Retained: {retained}')
         asyncio.create_task(pulse())
+
 
 async def down(client):
     global outages
@@ -39,35 +42,40 @@ async def down(client):
         client.down.clear()
         wifi_led(False)
         outages += 1
-        print('WiFi or broker is down.')
+        print("WiFi or broker is down.")
+
 
 async def up(client):
     while True:
         await client.up.wait()
         client.up.clear()
         wifi_led(True)
-        print('We are connected to broker.')
-        await client.subscribe('foo_topic', 1)
+        print("We are connected to broker.")
+        await client.subscribe("foo_topic", 1)
+
 
 async def main(client):
     try:
         await client.connect()
     except OSError:
-        print('Connection failed.')
+        print("Connection failed.")
         return
     for task in (up, down, messages):
         asyncio.create_task(task(client))
     n = 0
     while True:
         await asyncio.sleep(5)
-        print('publish', n)
+        print("publish", n)
         # If WiFi is down the following will pause for the duration.
-        await client.publish(TOPIC, '{} repubs: {} outages: {}'.format(n, client.REPUB_COUNT, outages), qos = 1)
+        await client.publish(
+            TOPIC, "{} repubs: {} outages: {}".format(n, client.REPUB_COUNT, outages), qos=1
+        )
         n += 1
 
+
 # Define configuration
-config['will'] = (TOPIC, 'Goodbye cruel world!', False, 0)
-config['keepalive'] = 120
+config["will"] = (TOPIC, "Goodbye cruel world!", False, 0)
+config["keepalive"] = 120
 config["queue_len"] = 1  # Use event interface with default queue
 
 # Set up client. Enable optional debug statements.
@@ -80,4 +88,3 @@ finally:  # Prevent LmacRxBlk:1 errors.
     client.close()
     blue_led(True)
     asyncio.new_event_loop()
-
--- mqtt_as/range_ex.py
+++ mqtt_as/range_ex.py
@@ -1,21 +1,22 @@
 # range_ex.py Test of asynchronous mqtt client with clean session False.
 # Extended version publishes SSID
-# (C) Copyright Peter Hinch 2017-2022.
+# (C) Copyright Peter Hinch 2017-2024.
 # Released under the MIT licence.
 
 # Now uses the event interface
 
 # Public brokers https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
 
-# This demo is for wireless range tests. If OOR the red LED will light.
+# This demo is for wireless range tests. If OOR the red WiFi ON LED will go out.
 # In range the blue LED will pulse for each received message.
 # Uses clean sessions to avoid backlog when OOR.
 
-# red LED: ON == WiFi fail
+# red LED: ON == WiFi OK
 # blue LED pulse == message received
 # Publishes connection statistics.
 
 from mqtt_as import MQTTClient, RP2
+
 if RP2:
     from sys import implementation
 from mqtt_local import wifi_led, blue_led, config
@@ -23,22 +24,24 @@ import uasyncio as asyncio
 import network
 import gc
 
-TOPIC = 'shed'  # For demo publication and last will use same topic
+TOPIC = "shed"  # For demo publication and last will use same topic
 
 outages = 0
 rssi = -199  # Effectively zero signal in dB.
+
 
 async def pulse():  # This demo pulses blue LED each time a subscribed msg arrives.
     blue_led(True)
     await asyncio.sleep(1)
     blue_led(False)
 
+
 # The only way to measure RSSI is via scan(). Alas scan() blocks so the code
 # causes the obvious uasyncio issues.
 async def get_rssi():
     global rssi
     s = network.WLAN()
-    ssid = config['ssid'].encode('UTF8')
+    ssid = config["ssid"].encode("UTF8")
     while True:
         try:
             rssi = [x[3] for x in s.scan() if x[0] == ssid][0]
@@ -46,10 +49,12 @@ async def get_rssi():
             rssi = -199
         await asyncio.sleep(30)
 
+
 async def messages(client):
     async for topic, msg, retained in client.queue:
         print(f'Topic: "{topic.decode()}" Message: "{msg.decode()}" Retained: {retained}')
         asyncio.create_task(pulse())
+
 
 async def down(client):
     global outages
@@ -58,39 +63,44 @@ async def down(client):
         client.down.clear()
         wifi_led(False)
         outages += 1
-        print('WiFi or broker is down.')
+        print("WiFi or broker is down.")
+
 
 async def up(client):
     while True:
         await client.up.wait()
         client.up.clear()
         wifi_led(True)
-        print('We are connected to broker.')
-        await client.subscribe('foo_topic', 1)
+        print("We are connected to broker.")
+        await client.subscribe("foo_topic", 1)
+
 
 async def main(client):
     try:
         await client.connect()
     except OSError:
-        print('Connection failed.')
+        print("Connection failed.")
         return
     asyncio.create_task(up(client))
     asyncio.create_task(down(client))
     asyncio.create_task(messages(client))
     n = 0
-    s = '{} repubs: {} outages: {} rssi: {}dB free: {}bytes discards: {}'
+    s = "{} repubs: {} outages: {} rssi: {}dB free: {}bytes discards: {}"
     while True:
         await asyncio.sleep(5)
         gc.collect()
         m = gc.mem_free()
-        print('publish', n)
+        print("publish", n)
         # If WiFi is down the following will pause for the duration.
-        await client.publish(TOPIC, s.format(n, client.REPUB_COUNT, outages, rssi, m, client.queue.discards), qos = 1)
+        await client.publish(
+            TOPIC, s.format(n, client.REPUB_COUNT, outages, rssi, m, client.queue.discards), qos=1
+        )
         n += 1
 
+
 # Define configuration
-config['will'] = (TOPIC, 'Goodbye cruel world!', False, 0)
-config['keepalive'] = 120
+config["will"] = (TOPIC, "Goodbye cruel world!", False, 0)
+config["keepalive"] = 120
 config["queue_len"] = 4  # Use event interface
 
 # Set up client. Enable optional debug statements.
@@ -99,7 +109,7 @@ client = MQTTClient(config)
 
 # Currently (Apr 22) this task causes connection periodically to be dropped on Arduino Nano Connect
 # It does work on Pico W
-if not RP2 or 'Pico W' in implementation._machine:
+if not RP2 or "Pico W" in implementation._machine:
     asyncio.create_task(get_rssi())
 try:
     asyncio.run(main(client))
@@ -107,4 +117,3 @@ finally:  # Prevent LmacRxBlk:1 errors.
     client.close()
     blue_led(True)
     asyncio.new_event_loop()
-
